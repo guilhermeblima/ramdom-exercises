efa7afd61c43e55e37248c62609466cb2b7030af
Guilherme Lima <guilhermelima.iba@gmail.com>
2017-11-23 12:55:55 +1300

    funcao de verificao do limite

--- c++/chaveSegurancaPrograma.cpp
+++ c++/chaveSegurancaPrograma.cpp
@@ -9,6 +9,37 @@ int calculaRestoProdutoAnoNascimentoCPF(int anoNascimento, int cpf){
     return (anoNascimento * cpf) % 100; 
 }
 
+int calculaDigitosLimiteGeneroInadimplencia(char genero, int limite, bool isInadimplente){
+    int primeiroDigito, segundoDigito;
+    if(!isInadimplente && limite <10000 ){
+        primeiroDigito = limite / 1000;
+        if(genero == "M"){ 
+            segundoDigito = 0;
+        }else{
+            segundoDigito = 1;
+        }
+    }else if(isInadimplente && limite < 10000){
+    primeiroDigito = limite % 1000;
+        if(genero == "M"){ 
+            segundoDigito = 2;
+        }else{
+            segundoDigito = 3;
+        }
+    }else{
+        primeiroDigito = 9;
+        if(genero == "M" && !isInadimplente){ 
+            segundoDigito = 5;
+        }else if(genero == "F" && !isInadimplente){
+            segundoDigito = 6;
+        }else if(genero == "M" && isInadimplente){
+            segundoDigito = 7;
+        }else{
+            segundoDigito = 8;
+        }
+    }
+    return 10 * primeiroDigito + segundoDigito;
+}
+
 int main() {
 	string nomeCliente, nomePai, nomeMae;
 	char genero;
